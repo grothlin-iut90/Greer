767cc76009cfac94ff3b76bc83496edf19bc180e
grothlin-iut90 <146071993+grothlin-iut90@users.noreply.github.com>
2024-10-02 20:13:05 +0200

    test

---
+++ src/datasource/controller.js
@@ -0,0 +1,63 @@
+import {items, shopusers, bankaccounts, transactions} from './data.js'
+import {v4 as uuidv4} from 'uuid'
+/* controllers: les fonctions ci-dessous doivent mimer ce que renvoie l'API en fonction des requêtes possibles.
+
+  Dans certains cas, ces fonctions vont avoir des paramètres afin de filtrer les données qui se trouvent dans data.js
+  Ces paramètres sont généralement les mêmes qu'ils faudrait envoyer à l'API, mais pas forcément.
+
+  Exemple 1 : se loguer auprès de la boutique
+ */
+
+function shopLogin(data) {
+  if ((!data.login) || (!data.password)) return {error: 1, status: 404, data: 'aucun login/pass fourni'}
+  // pour simplifier : test uniquement le login
+  let user = shopusers.find(e => e.login === data.login)
+  if (!user) return {error: 1, status: 404, data: 'login/pass incorrect'}
+  // générer un uuid de session pour l'utilisateur si non existant
+  if (!user.session) {
+    user.session = uuidv4()
+  }
+  // retourne uniquement les champs nécessaires
+  let u = {
+    _id: user._id,
+    name: user.name,
+    login: user.login,
+    email: user.email,
+    session: user.session
+  }
+  return {error: 0, status: 200, data: u}
+}
+
+function getAllViruses() {
+  return {error: 0, data: items}
+}
+
+function getAccountAmount(number) {
+  //alert("Numéro de compte : " + number);
+  if (!number) {
+    return { error: 1, status: 404, data: 'aucun numéro de compte fourni' };
+  }
+  let account = bankaccounts.find(e => e.number === number);
+  //alert("Compte trouvé : " + JSON.stringify(account));
+  if (account) {
+    return { error: 0, data: account.amount };
+  }
+  return { error: 1, status: 404, data: 'aucun compte trouvé' };
+}
+
+function getTransactionsForAccount(accountNumber) {
+  const account = bankaccounts.find(acc => acc.number === accountNumber);
+  if (!account) {
+    return { error: 1, message: "Compte non trouvé" };
+  }
+  const accountId = account._id;
+  const relatedTransactions = transactions.filter(tx => tx.account === accountId);
+  return { error: 0, data: relatedTransactions };
+}
+
+export default{
+  shopLogin,
+  getAllViruses,
+  getAccountAmount,
+  getTransactionsForAccount,
+}
---
+++ src/views/VirusesView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Les virus</h1>
+    <p>Le tableau dans le store : {{ viruses }}</p>
+    <p>sous forme de liste avec certains champs</p>
+    <ul>
+      <li v-for="(virus, index) in viruses" :key="index">{{virus.name}} : {{virus.price}}</li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+
+import {mapState} from 'vuex'
+export default {
+  name: 'VirusesView',
+  data: () => ({
+
+  }),
+  computed: {
+    ...mapState(['viruses'])
+  }
+
+}
+</script>
